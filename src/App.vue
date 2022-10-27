9ead87136c2f04cc227a0e87802c28358c3dd652
yzchen <yzchen@amazon.com>
2022-10-27 14:41:39 +0800

    change setting

--- src/App.vue
+++ src/App.vue
@@ -50,12 +50,6 @@ export default {
     }
   },
   mounted() {
-    Amplify.configure({
-      aws_appsync_graphqlEndpoint: 'https://x53vqtsmlrcbvfxbsdgizfpk6a.appsync-api.ap-northeast-1.amazonaws.com/graphql',
-      aws_appsync_region: 'ap-northeast-1',
-      aws_appsync_authenticationType: 'API_KEY',
-      aws_appsync_apiKey: 'da2-oixabp3vinbkbjv43gvnfd35he'
-    });
     const subscription = API.graphql(
       graphqlOperation(subscriptions.onCastVote)
     ).subscribe({
